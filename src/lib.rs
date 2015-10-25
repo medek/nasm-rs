5ff1fc4957157f8531ad6daed13e0fe8ec78d8ee
Gavin Massey <mdk@mystacktrace.org>
2015-10-25 22:39:06 +0000

    Updated code for rust 1.3.0
    Updated code so it compiles against 1.3.0.
    
    Added basic triple parsing for telling nasm output format.
    Added check for whether to tell nasm to compile with debug info.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,41 @@
-#![feature(if_let)]
+use std::env;
+use std::process::Command;
+use std::process::Stdio;
+use std::path::Path;
 
-use std::os;
-use std::io::Command;
-use std::io::process::InheritFd;
+fn x86_triple(os: &str) -> &'static str {
+    match os {
+        "linux" => "-felf32",
+        "macos" => "-fmachos32",
+        "windows" => "-fwin32",
+        _ => ""
+    }
+}
+
+fn x86_64_triple(os: &str) -> &'static str {
+    match os {
+        "linux" => "-felf64",
+        "macos" => "-felf64",
+        "windows" => "-fwin64",
+        _ => ""
+    }
+}
+
+fn parse_triple(trip: &str) -> &'static str {
+    let parts = trip.split("-").collect::<Vec<&str>>();
+    // ARCH-VENDOR-OS-ENVIRONMENT
+    // or ARCH-VENDOR-OS
+    // we don't care about environ so doesn't matter if triple doesn't have it
+    if parts.len() < 3 {
+        return ""
+    }
+
+    match parts[0] {
+        "x86_64" => x86_64_triple(&parts[2]),
+        "x86" => x86_triple(&parts[2]),
+        _ => ""
+    }
+}
 
 /// # Example
 ///
@@ -14,41 +47,44 @@ pub fn compile_library(output: &str, files: &[&str]) {
 
     assert!(output.ends_with(".a"));
 
-    let target = os::getenv("TARGET").unwrap();
+    let target = env::var("TARGET").unwrap();
 
-    let opt_level = os::getenv("OPT_LEVEL").unwrap();
+    let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let mut args:Vec<&str> = Vec::new();
+    args.push(parse_triple(&target));
 
-    let mut cmd = Command::new("nasm");
+    if env::var_os("DEBUG").is_some() {
+        args.push("-g");
+    }
 
-    cmd.arg("-felf64");
+    let src = Path::new(&cargo_manifest_dir);
 
-    let src = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap());
-
-    let dst = Path::new(os::getenv("OUT_DIR").unwrap());
+    let dst = Path::new(&out_dir);
 
     let mut objects = Vec::new();
 
     for file in files.iter() {
         let obj = dst.join(*file).with_extension("o");
+        let mut cmd = Command::new("nasm");
+        cmd.args(&args[..]);
+        std::fs::create_dir_all(&obj.parent().unwrap()).unwrap();
 
-        std::io::fs::mkdir_recursive(&obj.dir_path(), std::io::USER_RWX).unwrap();
-
-        run(cmd.clone().arg(src.join(*file)).arg("-o").arg(&obj));
-
+        run(cmd.arg(src.join(*file)).arg("-o").arg(&obj));
         objects.push(obj);
     }
 
-    run(Command::new(ar()).arg("crus").arg(dst.join(output)).args(objects.as_slice()));
+    run(Command::new(ar()).arg("crus").arg(dst.join(output)).args(&objects[..]));
 
     println!("cargo:rustc-flags=-L {} -l {}:static",
              dst.display(),
-             output.slice(3, output.len() - 2));
+             &output[3..output.len() - 2]);
 }
 
 fn run(cmd: &mut Command) {
-    println!("running: {}", cmd);
+    println!("running: {:?}", cmd);
 
-    let status = match cmd.stdout(InheritFd(1)).stderr(InheritFd(2)).status() {
+    let status = match cmd.stdout(Stdio::inherit()).stderr(Stdio::inherit()).status() {
         Ok(status) => status,
 
         Err(e) => panic!("failed to spawn process: {}", e),
@@ -60,5 +96,5 @@ fn run(cmd: &mut Command) {
 }
 
 fn ar() -> String {
-    os::getenv("AR").unwrap_or("ar".to_string())
+    env::var("AR").unwrap_or("ar".to_string())
 }
